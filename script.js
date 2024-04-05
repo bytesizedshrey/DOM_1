c0460bbf9d1531fd3f706853239330640b5d3d80
Shreyash Gajbhiye <shreyashgajbhiye@Shreyashs-MacBook-Air.local>
2024-04-05 10:12:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/* tagName : 
+let firstElement = document.querySelector("p");
+console.dir(firstElement)
+*/
+
+// console.dir(document.body.firstChild);
+
+let div = document.querySelector('div')
+console.dir(div);
+
